2c74740ff320c520fd90932357bb172219e714fb
Repertoi-e <dsotirov@windowslive.com>
2021-12-26 20:05:21 +0200

    Remove dub project

--- premake5_graphics.lua
+++ premake5_graphics.lua
@@ -31,30 +31,7 @@ project "graph"
 	-- Output dll in the same location as the driver
     targetdir("bin/%{cfg.buildcfg}/driver")
 	
-	filter "action:vs*"
-		pchheader "pch.h"
-		pchsource "%{prj.name}/src/pch.cpp"
-	filter {} -- @Platform Other compilers...
-	
     -- Unique PDB name each time we build (in order to support debugging while hot-swapping the game dll)
     filter "system:windows"
         symbolspath '$(OutDir)$(TargetName)-$([System.DateTime]::Now.ToString("ddMMyyyy_HHmmss_fff")).pdb'
         links { "dxgi.lib", "d3d11.lib", "d3dcompiler.lib", "d3d11.lib", "d3d10.lib" }
-
-project "engine"
-    location "%{prj.name}"
-    kind "SharedLib"
-
-	includedirs { "lstd/src", "driver/src" } 
-    links { "lstd" }
-    
-    common_settings()
-	
-	-- Output dll in the same location as the driver
-    targetdir("bin/%{cfg.buildcfg}/driver")
-	
-    -- Unique PDB name each time we build (in order to support debugging while hot-swapping the game dll)
-    filter "system:windows"
-        symbolspath '$(OutDir)$(TargetName)-$([System.DateTime]::Now.ToString("ddMMyyyy_HHmmss_fff")).pdb'
-        links { "dxgi.lib", "d3d11.lib", "d3dcompiler.lib", "d3d11.lib", "d3d10.lib" }
-	
